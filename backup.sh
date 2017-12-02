9b0364fede0ef4adf90c29f7287eca5d4dbabf0a
weka <markalarm@gmail.com>
2017-12-02 16:30:44 +0100

    auto

--- backup.sh
+++ backup.sh
@@ -8,3 +8,7 @@ mkdir ~/bckup/ranger
 cp ~/.config/ranger/rc.conf ~/bckup/ranger/rc.conf
 mkdir ~/bckup/nvim
 cp ~/.config/nvim/init.vim ~/bckup/nvim/init.vim
+
+git add .
+git commit
+git push -u origin master
