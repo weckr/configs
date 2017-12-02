7398a308576208c2d86451a8d038011f542b6a8b
weka <markalarm@gmail.com>
2017-12-02 16:13:35 +0100

    prvni komit

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BCKUPDIR=~/bckup/
+
+mkdir ~/bckup/i3
+cp ~/.config/i3/config ~/bckup/i3/config
+mkdir ~/bckup/ranger
+cp ~/.config/ranger/rc.conf ~/bckup/ranger/rc.conf
+mkdir ~/bckup/nvim
+cp ~/.config/nvim/init.vim ~/bckup/nvim/init.vim
